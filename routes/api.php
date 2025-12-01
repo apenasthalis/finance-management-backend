15e4f142ee3ce9237da0ebfcb8f1da7ddd5f5d86
thalis <1thalisgabriel1@gmail.com>
2025-12-01 11:02:19 -0300

    feat(devOps): testing CI/CD

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', [ UserController::class, 'index' ]);
 Route::post('/user', [ UserController::class, 'store' ]);
 Route::put('/user/{id}', [ UserController::class, 'update' ]);
+Route::put('/user/{id}', [ UserController::class, 'update' ]);
+
